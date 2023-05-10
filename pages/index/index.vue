d6392609b4fa33ffeccc6ad5ab73b7f5c034a984
瞿玲 <quling@dist.com.cn>
2023-05-10 22:15:32 +0800

    主页面样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,29 @@
 <template>
 	<view class="content">
 		<header class="seacher">
-			<u-button type="primary" text="确定"></u-button>
+			<view class="locate">
+				<u-icon name="map-fill" color="#f3950c" size="19"></u-icon>
+				<view>{{locateCity}}</view>
+			</view>
+			<view class="input-content">
+				<u-input placeholder="请输入内容" border="surround" clearable shape="circle"></u-input>
+			</view>
 		</header>
 		<section class="map-content">
 			<map class="map" :longitude="longitude" :latitude="latitude"></map>
+			<view class="scroll">
+				<u-scroll-list @right="right" @left="left" :indicatorWidth="0">
+					<view class="scroll-list" style="flex-direction: row;">
+						<view class="scroll-list__shops" v-for="(item, index) in list" :key="index"
+							:class="[(index === 5) && 'scroll-list__shops--no-margin-right']">
+							<view class="scroll-list__shops_item">
+								<image class="scroll-list__shops_item__image" :src="item.thumb"></image>
+								<view class="scroll-list__shops_item__des">{{ item.name }}</view>
+							</view>
+						</view>
+					</view>
+				</u-scroll-list>
+			</view>
 		</section>
 	</view>
 </template>
@@ -13,12 +32,30 @@
 	import { ref, onMounted } from 'vue'
 	const longitude = ref(0)
 	const latitude = ref(0)
-	const value = ref('11')
+	const locateCity = ref('成都') // 当前城市名
+	const list = ref(
+		[{
+			name: '门店1',
+			thumb: 'https://cdn.uviewui.com/uview/goods/1.jpg'
+		}, {
+			name: '门店2',
+			thumb: 'https://cdn.uviewui.com/uview/goods/2.jpg'
+		}, {
+			name: '门店3',
+			thumb: 'https://cdn.uviewui.com/uview/goods/6.jpg'
+		}, {
+			name: '门店4',
+			thumb: 'https://cdn.uviewui.com/uview/goods/5.jpg'
+		}, {
+			name: '门店5',
+			thumb: 'https://cdn.uviewui.com/uview/goods/2.jpg'
+		}])
 
 	onMounted(() => {
 		uni.getLocation({
 			type: 'wgs84',
 			success: res => {
+				console.log(res)
 				longitude.value = res.longitude
 				latitude.value = res.latitude
 			},
@@ -26,9 +63,16 @@
 			complete: () => {}
 		})
 	})
+
+	const left = () => {
+		console.log('left')
+	}
+	const right = () => {
+		console.log('right')
+	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	.content {
 		height: 100%;
 		width: 100%;
@@ -37,23 +81,80 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+
+		.seacher {
+			height: 60rpx;
+			padding: 10px 0;
+			width: 100%;
+			display: flex;
+			align-items: center;
+
+			.locate {
+				display: flex;
+				align-items: center;
+				margin: 0 20rpx;
+				font-size: $uni-font-size-sm;
+				white-space: nowrap;
+			}
+
+			.input-content {
+				flex: 1;
+				margin-right: 30rpx;
+
+				::v-deep .u-input {
+					height: 30rpx;
+				}
+			}
+		}
+
+		.map-content {
+			height: calc(100% - 80rpx);
+			width: 100%;
+			position: relative;
+
+			.map {
+				width: 100%;
+				height: 100%;
+			}
+
+			.scroll {
+				width: 100%;
+				position: absolute;
+				bottom: 10rpx;
+				left: 0;
+			}
+		}
 	}
 
-	.seacher {
-		height: 200rpx;
-		// color: $uni-color-primary;
-		width: 100%;
-	}
+	.scroll-list {
+		@include flex(column);
 
-	.map-content {
-		height: calc(100% - 200rpx);
-		width: 100%;
+		&__shops {
+			margin-right: 20rpx;
+			padding: 20rpx 0;
+			width: 300rpx;
+			height: 400rpx;
 
-	}
+			&_item {
+				background-color: $uni-bg-color;
+				border-radius: 20rpx;
+				box-shadow: 5rpx 5rpx 20rpx $uni-bg-color-mask ;
+				height: 100%;
 
-	.map {
-		width: 100%;
-		height: 100%;
+				&__image {
+					height: 310rpx;
+					width: 100%;
+					border-radius: 20rpx 20rpx 0 0;
+				}
+
+				&__des {
+					text-align: center;
+					font-size: $uni-font-size-base;
+					font-weight: bold;
+					border-radius: 20rpx;
+				}
+			}
+		}
 	}
 </style>
 
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -1,23 +1,46 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
 if (!Array) {
-  const _easycom_u_button2 = common_vendor.resolveComponent("u-button");
-  _easycom_u_button2();
+  const _easycom_u_icon2 = common_vendor.resolveComponent("u-icon");
+  const _easycom_u_input2 = common_vendor.resolveComponent("u-input");
+  const _easycom_u_scroll_list2 = common_vendor.resolveComponent("u-scroll-list");
+  (_easycom_u_icon2 + _easycom_u_input2 + _easycom_u_scroll_list2)();
 }
-const _easycom_u_button = () => "../../uni_modules/uview-plus/components/u-button/u-button.js";
+const _easycom_u_icon = () => "../../uni_modules/uview-plus/components/u-icon/u-icon.js";
+const _easycom_u_input = () => "../../uni_modules/uview-plus/components/u-input/u-input.js";
+const _easycom_u_scroll_list = () => "../../uni_modules/uview-plus/components/u-scroll-list/u-scroll-list.js";
 if (!Math) {
-  _easycom_u_button();
+  (_easycom_u_icon + _easycom_u_input + _easycom_u_scroll_list)();
 }
 const _sfc_main = {
   __name: "index",
   setup(__props) {
     const longitude = common_vendor.ref(0);
     const latitude = common_vendor.ref(0);
-    common_vendor.ref("11");
+    const locateCity = common_vendor.ref("成都");
+    const list = common_vendor.ref(
+      [{
+        name: "门店1",
+        thumb: "https://cdn.uviewui.com/uview/goods/1.jpg"
+      }, {
+        name: "门店2",
+        thumb: "https://cdn.uviewui.com/uview/goods/2.jpg"
+      }, {
+        name: "门店3",
+        thumb: "https://cdn.uviewui.com/uview/goods/6.jpg"
+      }, {
+        name: "门店4",
+        thumb: "https://cdn.uviewui.com/uview/goods/5.jpg"
+      }, {
+        name: "门店5",
+        thumb: "https://cdn.uviewui.com/uview/goods/2.jpg"
+      }]
+    );
     common_vendor.onMounted(() => {
       common_vendor.index.getLocation({
         type: "wgs84",
         success: (res) => {
+          console.log(res);
           longitude.value = res.longitude;
           latitude.value = res.latitude;
         },
@@ -27,17 +50,44 @@ const _sfc_main = {
         }
       });
     });
+    const left = () => {
+      console.log("left");
+    };
+    const right = () => {
+      console.log("right");
+    };
     return (_ctx, _cache) => {
       return {
         a: common_vendor.p({
-          type: "primary",
-          text: "确定"
+          name: "map-fill",
+          color: "#f3950c",
+          size: "19"
         }),
-        b: longitude.value,
-        c: latitude.value
+        b: common_vendor.t(locateCity.value),
+        c: common_vendor.p({
+          placeholder: "请输入内容",
+          border: "surround",
+          clearable: true,
+          shape: "circle"
+        }),
+        d: longitude.value,
+        e: latitude.value,
+        f: common_vendor.f(list.value, (item, index, i0) => {
+          return {
+            a: item.thumb,
+            b: common_vendor.t(item.name),
+            c: index,
+            d: common_vendor.n(index === 5 && "scroll-list__shops--no-margin-right")
+          };
+        }),
+        g: common_vendor.o(right),
+        h: common_vendor.o(left),
+        i: common_vendor.p({
+          indicatorWidth: 0
+        })
       };
     };
   }
 };
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "D:/学习/小程序/small-project/pages/index/index.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-1cf27b2a"], ["__file", "D:/学习/小程序/small-project/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
